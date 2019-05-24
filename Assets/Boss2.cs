739d7a54d72f84f649e56b356913593c24630744
Paul Kantoski <unkleyoyo@yahoo.com>
2019-05-23 23:54:54 -0400

    Still no boss complely working
    fixed up , animatons a bit bor thr boss that looks like the player, and setup the animator contoller, still trying to move boss using statemachines Meathod

---
+++ Assets/Boss2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2 : MonoBehaviour
+{
+    Animator anim;
+    SpriteRenderer SpriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
