0e480279499611d8ec94363b75c3bc548953a052
Paul Kantoski <unkleyoyo@yahoo.com>
2025-12-22 14:28:13 -0500

    fixed console errors, worked on player dealing damage and getting hurt, migrated to new InputSystem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,109 +1,146 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
-
+    [Header("Movement Settings")]
     public float moveSpeed;
-    public Rigidbody2D theRB;
+    public float bounceForce;
     public float jumpForce;
 
-    bool isGrounded;
+    [Header("Ground Check Settings")]
     public Transform groundCheckPoint;
     public LayerMask whatIsGround;
-
-     bool canDoupleJump;
-
-     SpriteRenderer theSR;
-     Animator anim;
-
+    [Header("Knockback Settings")]
     public float knockBackLength, knockBackForce;
-     float knockBackCounter; 
-
-    public float bounceForce;
-        
     public bool stopInput;
 
-    private void Awake()
+
+    public Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
+
+    Vector2 moveInput;
+    bool isGrounded;
+    bool canDoubleJump;
+    float knockBackCounter;
+    void Awake()
     {
         instance = this;
-    }
-
-    private void Start()
-    {
         anim = GetComponent<Animator>();
-        theSR = GetComponent<SpriteRenderer>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    private void Update()
+    void OnEnable()
+    {
+        var playerInput = GetComponent<PlayerInput>();
+        playerInput.onActionTriggered += OnActionTriggered;
+    }
+
+    void OnDisable()
+    {
+        var playerInput = GetComponent<PlayerInput>();
+        playerInput.onActionTriggered -= OnActionTriggered;
+    }
+
+    void Update()
     {
         if (knockBackCounter <= 0)
         {
-            theRB.linearVelocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.linearVelocity.y);
-
-            isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
-
-            if (isGrounded)
-            {
-                canDoupleJump = true;
-            }
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                if (isGrounded)
-                {
-                    theRB.linearVelocity = new Vector2(theRB.linearVelocity.x, jumpForce);
-                    //AudioManager.instance()
-                }
-                else
-                {
-                    if (canDoupleJump)
-                    {
-                        theRB.linearVelocity = new Vector2(theRB.linearVelocity.x, jumpForce);
-                        canDoupleJump = false;
-                        //AudioManager.instance()
-                    }
-                }
-            }
-
-            if (theRB.linearVelocity.x < 0)
-            {
-                theSR.flipX = true;
-            }
-            else if (theRB.linearVelocity.x > 0)
-            {
-                theSR.flipX = false;
-            }
-            else
-            {
-                knockBackCounter -= Time.deltaTime;
-                if (!theSR.flipX)
-                {
-                    theRB.linearVelocity = new Vector2(-knockBackForce, theRB.linearVelocity.y);
-                }
-                else
-                {
-                    theRB.linearVelocity = new Vector2(knockBackForce, theRB.linearVelocity.y);
-                }
-            }
+            HandleMovement();
+            CheckGroundStatus();
         }
+        else
+        {
+            HandleKnockBack();
+        }
+        UpdateAnimation();
+    }
+    void HandleMovement()
+    {
+        rb.linearVelocity = new Vector2(moveSpeed * moveInput.x, rb.linearVelocity.y);
 
-        anim.SetFloat("moveSpeed", Mathf.Abs(theRB.linearVelocity.x));
+        if (moveInput.x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (moveInput.x > 0)
+        {
+            sr.flipX = false;
+        }
+    }
+    void CheckGroundStatus()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+    }
+    void HandleKnockBack()
+    {
+        knockBackCounter -= Time.deltaTime;
+        float knockBackDirection = Mathf.Sign(moveInput.x);
+        rb.linearVelocity = new Vector2(knockBackDirection * knockBackForce, rb.linearVelocity.y);
+    }
+
+
+    void UpdateAnimation()
+    {
+        anim.SetFloat("moveSpeed", Mathf.Abs(rb.linearVelocity.x));
         anim.SetBool("isGrounded", isGrounded);
     }
+    void OnActionTriggered(InputAction.CallbackContext context)
+    {
+        switch (context.action.name)
+        {
+            case "Move":
+                moveInput = context.ReadValue<Vector2>();
+                print("isMoving");
+                break;
+            case "Jump":
+                if (context.performed)
+                { 
+                    HandleJump();
+                }
+                break;
+            default:
+                print($"Unhandled action: {context.action.name}");
+                break;
+        }
+    }
+    void HandleJump()
+    {
+        if (isGrounded)
+            Jump();
+            //AudioManager.instance()
+        
+        else if (canDoubleJump)
+        {
+            Jump();
+            canDoubleJump = false;
+            //AudioManager.instance()
+        }
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
     public void KnockBack()
     {
         knockBackCounter = knockBackLength;
-        theRB.linearVelocity = new Vector2(0f, knockBackForce);
-
+        rb.linearVelocity = new Vector2(0f, knockBackForce);
 
         anim.SetTrigger("Hurt");
     }
 
     public void Bounce()
     {
-        theRB.linearVelocity = new Vector2(theRB.linearVelocity.x, bounceForce);
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, bounceForce);
         //AudioManager.intance.PlaySFX();
-    } 
+    }
 }
-   
