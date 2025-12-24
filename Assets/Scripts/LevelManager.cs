b29cf1c226f42b424bb32c5a0c87753d8661f5f9
Paul Kantoski <unkleyoyo@yahoo.com>
2025-12-24 10:23:28 -0500

    continued to work on bug fixes, made UI work with UGUI next step switching over to UI ToolKit.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
 
     public float waitToRespawn;
+
+    public int coinCollected; 
 
     public float timeInLevel;
 
@@ -50,6 +51,7 @@ public class LevelManager : MonoBehaviour
     {
         //AudioManager.instance.PlayVictoryMusic()
         PlayerController.instance.stopInput = true;
+        
 
         yield return new WaitForSeconds(2f);
     }
